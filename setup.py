e95e326baa78366213c39400ed966f1e50ff686d
pivotsengineer <engineer@pivotsdoo.com>
2024-08-09 16:48:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,97 +33,33 @@ for x in range(1,4):
 		break
 
 for x in range(1,4):
-	if os.system("sudo pip3 install -U pip") == 0:
+	if os.system("sudo apt-get install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential") == 0:
 		break
 
 for x in range(1,4):
-	if os.system("sudo apt-get install -y python-dev python3-pip libfreetype6-dev libjpeg-dev build-essential") == 0:
+	if os.system("sudo apt-get install -y python3-flask python3-flask-cors python3-websockets") == 0:
 		break
 
 for x in range(1,4):
-	if os.system("sudo -H pip3 install --upgrade luma.oled") == 0:
+	if os.system("sudo apt-get install -y python3-opencv python3-numpy python3-imutils python3-zmq python3-psutil") == 0:
 		break
 
 for x in range(1,4):
-	if os.system("sudo apt-get install -y i2c-tools") == 0:
+	if os.system("sudo apt-get install -y python3-smbus i2c-tools") == 0:
 		break
 
 for x in range(1,4):
-	if os.system("sudo apt-get install -y python3-smbus") == 0:
+	if os.system("sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev") == 0:
 		break
 
 for x in range(1,4):
-	if os.system("sudo pip3 install icm20948") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install flask") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install flask_cors") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install websockets") == 0:
-		break
-
-try:
-	replace_num("/boot/config.txt",'#dtparam=i2c_arm=on','dtparam=i2c_arm=on')
-except:
-	print('try again')
-
-try:
-	replace_num("/boot/config.txt",'[all]','[all]\ngpu_mem=128')
-except:
-	print('try again')
-
-try:
-	replace_num("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1\nstart_x=1')
-except:
-	print('try again')
-
-try:
-	replace_num("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1')
-except:
-	print('try again')
-
-
-for x in range(1,4):
-	if os.system("sudo pip3 install opencv-contrib-python==3.4.11.45") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple opencv-contrib-python==3.4.11.45") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 uninstall -y numpy") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install numpy==1.21") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple numpy==1.21") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install imutils zmq pybase64 psutil") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple imutils zmq pybase64 psutil") == 0:
+	if os.system("sudo apt-get install -y python3-pi-ina219") == 0:
 		break
 
 for x in range(1,4):
 	if os.system("sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
 		break
 
-for x in range(1,4):
-	if os.system("sudo pip3 install pi-ina219") == 0:
-		break
-
-        
 for x in range(1,4):
 	if os.system("cd " + thisPath + " && cd .. && sudo git clone https://github.com/oblique/create_ap") == 0:
 		break
